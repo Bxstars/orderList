6d8c3202646be3c00c23a95bc8dfb15e4c5093fe
Bxstars <bxstars4@gmail.com>
2024-12-13 15:18:58 -0400

    feat: creating nav-user

---
+++ src/app/shared/components/nav-user/nav-user.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-user.component.html',
+  styleUrl: './nav-user.component.css'
+})
+export class NavUserComponent {
+  today: any = "";
+
+  ngOnInit(): void {
+    let date = new Date();
+    this.today = date.toLocaleDateString(
+      'en-US',
+      {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      }
+    )
+  }
+
+
+}
