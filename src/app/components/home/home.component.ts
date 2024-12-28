703e0693020798212d71b872b1b5c7e05db22bc2
Bxstars <bxstars4@gmail.com>
2024-12-27 21:06:45 -0400

    fix: changed the dimensions of the home screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
-import { CardsComponent } from "../../shared/components/cards/cards.component";
-import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { CarouselComponent } from "../../shared/components/carousel/carousel.component";
-import { NavUserComponent } from "../../shared/components/nav-user/nav-user.component";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CardsComponent, FooterComponent, CarouselComponent, NavUserComponent],
+  imports: [CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
