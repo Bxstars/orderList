882f9834c7c1379cbd67b55da4fd021abc957afa
Bxstars <bxstars4@gmail.com>
2024-12-27 21:05:45 -0400

    feat: adds the functionality to change order status

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 
 export interface TableColumn {
@@ -11,7 +13,7 @@ export interface TableColumn {
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [MatTableModule, CommonModule],
+  imports: [MatTableModule, CommonModule, MatSelectModule, MatOptionModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
@@ -23,6 +25,32 @@ export class TableComponent {
 
   get displayedColumns(): string[] {
     return this.columns.map(column => column.columnDef)
+  }
+
+  statusList = [
+    { value: 'new', name: 'New' },
+    { value: 'on process', name: 'On Process' },
+    { value: 'delivery', name: 'Delivery' },
+    { value: 'done', name: 'Done' }
+  ];
+
+  updateStatus(row: any, newValue: number) {
+    row.status = newValue;
+  }
+
+  getStatusClass(status: string): string {
+    switch (status) {
+      case 'new':
+        return 'status-new';
+      case 'on process':
+        return 'status-on-process';
+      case 'delivery':
+        return 'status-delivery';
+      case 'done':
+        return 'status-done';
+      default:
+        return '';
+    }
   }
 
 }
